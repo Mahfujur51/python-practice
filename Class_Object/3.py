17306796ade9e38c58a540573233e9f24f00005f
Mahfujur51 <overcastmoon@gmail.com>
2020-07-25 08:09:09 +0600

    class and object

---
+++ Class_Object/3.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def my_function(self):
+        print("Hello my name is "+ self.name)
+p1=Person("Mahfujur",36)
+p1.my_function()
