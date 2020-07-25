d3a70e63cbea9763b19e24b8f3368816de8643ae
Mahfujur51 <overcastmoon@gmail.com>
2020-07-25 08:38:30 +0600

    inheritance

---
+++ Inheritance/1.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self,fname,lname):
+        self.fname=fname
+        self.lname=lname
+
+    def printname(self):
+        print(self.fname,self.lname)
+x=Person("Mahfujur","Rahman")
+x.printname()
